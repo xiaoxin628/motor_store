1a2f10a57a0aa3cf2b612b379d5ad0927cf9658b
Shuzu Li <lishuzu@gmail.com>
2025-07-09 11:26:15 +1200

    install jsbundle, esbuild and react

--- Gemfile
+++ Gemfile
@@ -61,3 +61,5 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
 end
+
+gem "jsbundling-rails", "~> 1.3"
---
+++ app/javascript/controllers/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = ()=> {
+    return (
+        <div>
+            App1111111111111sss
+        </div>
+    );
+}
+
+export default App;
